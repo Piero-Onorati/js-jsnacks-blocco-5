d003c6977858139d8f4d3fbbc5b4f2c8478b7107
Piero <piero.onorati@virgilio.it>
2021-06-15 15:40:58 +0200

    complete

--- js/script3.js
+++ js/script3.js
@@ -16,20 +16,25 @@ for (var i = 0; i < zucchine.length; i++) {
     if (zucchine[i].lunghezza <= 15) {
         zucchineCorte.push(zucchine[i]);
         pesoTot1 += zucchine[i].peso;
-        document.getElementById('zucchinecorte').innerHTML += 'varietà : ' + zucchine[i].varieta + ', peso :' + zucchine[i].peso + ' , lunghezza : ' + zucchine[i].lunghezza + '<br>';
+
+        //output
+        document.getElementById('zucchinecorte').innerHTML += 'varietà : ' + zucchine[i].varieta + ' , peso: ' + zucchine[i].peso + 'g, lunghezza : ' + zucchine[i].lunghezza + 'cm'+ '<br>';
 
     }else{
         zucchineLunghe.push(zucchine[i]);
         pesoTot2 += zucchine[i].peso;
-        document.getElementById('zucchinelunghe').innerHTML += 'varietà : ' + zucchine[i].varieta + ', peso :' + zucchine[i].peso + ' , lunghezza : ' + zucchine[i].lunghezza + '<br>';
+
+        //output
+        document.getElementById('zucchinelunghe').innerHTML += 'varietà : ' + zucchine[i].varieta + ', peso: ' + zucchine[i].peso + 'g, lunghezza : ' + zucchine[i].lunghezza + 'cm'+ '<br>';
     }
 }
 
 // console.log(zucchineCorte);
 // console.log(zucchineLunghe);
-
 // console.log(pesoTot1);
 // console.log(pesoTot2);
 
+
+//output
 document.getElementById('pesozucchineCorte').innerHTML = 'Il peso totale di tutte le zucchine corte è: ' + pesoTot1 + ' grammi';
 document.getElementById('pesozucchineLunghe').innerHTML = 'Il peso totale di tutte le zucchine lunghe è: ' + pesoTot2 + ' grammi';
